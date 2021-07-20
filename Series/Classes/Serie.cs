86abd692cbe7732e4419ab65b1172fb45e098b19
EnzoSchetine <enzoschetine@hotmail.com>
2021-07-20 12:01:40 -0300

    Implementação Inicial

---
+++ Series/Classes/Serie.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Series
+{
+    class Serie : EntidadeBase
+    {
+        // Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        private bool Excluido { get; set; }
+
+        // Métodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+            Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Genero " + Genero + Environment.NewLine;
+            retorno += "Título " + Titulo + Environment.NewLine;
+            retorno += "Descrição " + Descricao + Environment.NewLine;
+            retorno += "Ano de Início " + Ano + Environment.NewLine;
+            retorno += "Excluído " + Excluido;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return Titulo;
+        }
+        public int retornaId()
+        {
+            return Id;
+        }
+        public bool retornaExcluido()
+        {
+            return Excluido;
+        }
+        public void Excluir()
+        {
+            Excluido = true;
+        }
+    }
+}
